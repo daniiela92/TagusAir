f05a09da293f1faeae975f3e0ce831718a964761
Daniela Pais <dannyelarbpais@hotmail.com>
2026-09-23 19:21:55 +0100

    Add user registration

---
+++ TagusAir/Models/RegisterNewUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagusAir.Models
+{
+    public class RegisterNewUserViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [MaxLength(20)]
+        public string? Passport { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string Confirm { get; set; }
+
+    }
+}
